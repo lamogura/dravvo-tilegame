2ea0aba7ed86bdc2850e6fc208409dcf2dd52a51
Jeremy Anderson <holdemplease@hotmail.com>
2013-01-24 00:35:02 +0900

    constants now define thingys

--- tilegame/GameConstants.h
+++ tilegame/GameConstants.h
@@ -6,9 +6,8 @@
 //
 //
 
-#import <Foundation/Foundation.h>
-
-int kInitShurikens = 10;
-int kInitMissiles = 10;
-int kMaxMelons = 5;  // defined as kNumMelons in DVConstants.h
-int kTimeStepSeconds = 1;  // time step for sampling rate and oponnent turn re-play animation rate
+#define kNumMelons 5
+#define kInitShurikens 10
+#define kInitMissiles 10
+#define kMaxMelons 5
+#define kTimeStepSeconds 1  // time step for sampling rate and oponnent turn re-play animation rate
