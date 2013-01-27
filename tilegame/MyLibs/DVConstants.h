ecfcd27c323ad24ae7c8949d316507d3b4edc691
Justin Kovalchuk <xentropic@gmail.com>
2013-01-27 23:30:25 +0900

    using remote server

--- tilegame/MyLibs/DVConstants.h
+++ tilegame/MyLibs/DVConstants.h
@@ -17,7 +17,7 @@
 #define kCurrentGameIDKey @"gameID" // used to save/fetch from NSUserDefaults
 #define kDeviceToken @"deviceToken" // user to save the device token for APNS
 
-//#define kBaseURL @"http://dravvo.ap01.aws.af.cm"
+#define kBaseURL @"http://dravvo.ap01.aws.af.cm"
 //#define kBaseURL @"http://192.168.1.103:3000"
-#define kBaseURL @"http://192.168.20.2:3000"
+//#define kBaseURL @"http://192.168.20.2:3000"
 //#define kBaseURL @"http://localhost:3000"
